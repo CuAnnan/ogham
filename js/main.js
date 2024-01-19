2e28523614a4b8c3226d7a883efab9db62642acd
Wing <wing@busschots.ie>
2024-01-19 07:30:07 +0000

    Dipthong toggle implemented Reverse translation implemented

--- js/main.js
+++ js/main.js
@@ -4,35 +4,92 @@
   let start = '᚛';
   let end = '᚜'
 
-  let $output;
-  let $input;
-  window.addEventListener('load', function(){
-    $output = document.getElementById('output');
-    $input = document.getElementById('plaintext');
-    $input.addEventListener('keyup', function(){
-      let oghamScript = start;
+  let $latinInput;
+  let $oghamOutput;
+  let $dipthongs;
 
-      let fixedText = this.value
+  let $oghamInput;
+  let $latinOutput;
+
+
+  function latinToOgham()
+  {
+    let oghamScript = start;
+    let fixedText = $latinInput.value;
+    if($dipthongs.checked === true)
+    {
+      fixedText = fixedText
         .toLowerCase()
+        .replaceAll('ng', '?')
         .replaceAll('ea', '1')
         .replaceAll('oi', '2')
         .replaceAll('ui', '3')
         .replaceAll('ia', '4')
         .replaceAll('ae', '5');
-      for(let i = 0; i < fixedText.length; i++)
+    }
+    for(let i = 0; i < fixedText.length; i++)
+    {
+      let char = fixedText.charAt(i);
+      if(char === '.')
       {
-        let char = fixedText.charAt(i);
-        if(char === '.')
-        {
-          oghamScript += end+'<br/>'+start;
-        }
-        let pos = alphabet.indexOf(char);
-        let oghamChar = ogham.charAt(pos);
-        oghamScript += oghamChar;
+        oghamScript += end+'<br/>'+start;
       }
-      oghamScript += end;
-      output.innerHTML = oghamScript;
-    });
+      let pos = alphabet.indexOf(char);
+      let oghamChar = ogham.charAt(pos);
+      oghamScript += oghamChar;
+    }
+    oghamScript += end;
+    $oghamOutput.innerHTML = oghamScript;
+  }
+
+  function oghamToLatin()
+  {
+    let plainText = '';
+    for(let i = 0; i < $oghamInput.value.length; i++)
+    {
+      let stave = $oghamInput.value.charAt(i);
+      if(stave !== start)
+      {
+        if(stave === end)
+        {
+          plainText += '<br/>';
+        }
+        else
+        {
+          let pos = ogham.indexOf(stave);
+          let letter = alphabet.charAt(pos);
+          plainText += letter;
+        }
+      }
+      // fixedText = fixedText
+      //         .toLowerCase()
+      //         .replaceAll('ea', '1')
+      //         .replaceAll('oi', '2')
+      //         .replaceAll('ui', '3')
+      //         .replaceAll('ia', '4')
+      //         .replaceAll('ae', '5');
+      plainText = plainText
+        .replaceAll('?', 'ng')
+        .replaceAll('1', 'ea')
+        .replaceAll('2', 'oi')
+        .replaceAll('3', 'ui')
+        .replaceAll('4', 'ia')
+        .replaceAll('5', 'ae');
+
+
+      $latinOutput.innerHTML = plainText;
+    }
+  }
+
+  window.addEventListener('load', function(){
+    $oghamOutput = document.getElementById('oghamOutput');
+    $latinInput = document.getElementById('plaintext');
+    $dipthongs = document.getElementById('replaceDipthongs');
+    $oghamInput = document.getElementById('ogham');
+    $latinOutput = document.getElementById('plainTextOutput');
+    $latinInput.addEventListener('keyup', latinToOgham);
+    $dipthongs.addEventListener('change', latinToOgham);
+    $oghamInput.addEventListener('keyup', oghamToLatin);
   });
 
 })();
